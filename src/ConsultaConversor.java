7dd90c4e0964bbc0e07c904f10311c330ca7c8cb
Gonzalo Bazzani <gonzalobazzani@gmail.com>
2025-05-11 19:06:09 -0300

    Obtención y muestra de datos desde la APIs

---
+++ src/ConsultaConversor.java
@@ -0,0 +1,42 @@
+import com.google.gson.Gson;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaConversor {
+
+        private double valorConversion = 0;
+
+        public ConversorMoneda solicitarConversionMoneda(String moneda){
+            URI direccion = URI.create("https://v6.exchangerate-api.com/v6/6d13fd38e9c26a14c0c0ac65/latest/"+moneda);
+
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(direccion)
+                    .build();
+
+            try {
+                HttpResponse<String> response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+
+                return new Gson().fromJson(response.body(),ConversorMoneda.class);
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                throw new RuntimeException("No logró realizar la conversión");
+            }
+        }
+
+    public String conversionMoneda(String monedaConsultada, String monedaRespuesta,
+                                   double cantidadAConvertir, double valorMoneda){
+
+        double monedaConvertida = valorMoneda * cantidadAConvertir;
+
+        String respuesta = "El valor de " + cantidadAConvertir + " [" + monedaConsultada + "] corresponde al valor final de =>> " +
+                monedaConvertida + "[" + monedaRespuesta + "]";
+
+        return respuesta;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,133 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int opcion = 0;
+
+        String menu = """
+                    ****************************************
+                    1) Dolar =>> Peso argentino
+                    2) Peso argentino =>> Dolar
+                    3) Dolar =>> Real brasileño
+                    4) Real brasileño =>> Dolar
+                    5) Dola =>> Peso colombiano
+                    6) Peso colombiano =>> Dolar
+                    7) Salir
+                    
+                    Elija una opción válida:
+                    ****************************************
+                    """;
+
+        String solicitudValor = "Ingrese el valor que desea convertir: ";
+
+        ConsultaConversor consulta = new ConsultaConversor();
+
+        while(opcion != 7 ){
+
+            double cantidad;
+
+            System.out.println(" \n Sea bienvenido/a al Conversor de Moneda =] \n");
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+
+            switch (opcion){
+                case 1:
+                    System.out.println(solicitudValor);
+                    cantidad = teclado.nextDouble();
+
+                    ConversorMoneda solicitarConversion = consulta.solicitarConversionMoneda("USD");
+                    double valorMoneda = solicitarConversion.conversion_rates().get("ARS");
+
+                    String conversionExitosa = consulta.conversionMoneda("USD",
+                            "ARS",
+                            cantidad,
+                            valorMoneda);
+
+                    System.out.println(conversionExitosa);
+                    break;
+                case 2:
+                    System.out.println(solicitudValor);
+                    cantidad = teclado.nextDouble();
+
+                    solicitarConversion = consulta.solicitarConversionMoneda("ARS");
+                    valorMoneda = solicitarConversion.conversion_rates().get("USD");
+
+                    conversionExitosa = consulta.conversionMoneda("ARS",
+                            "USD",
+                            cantidad,
+                            valorMoneda);
+
+                    System.out.println(conversionExitosa);
+                    break;
+                case 3:
+                    System.out.println(solicitudValor);
+                    cantidad = teclado.nextDouble();
+
+                    solicitarConversion = consulta.solicitarConversionMoneda("USD");
+                    valorMoneda = solicitarConversion.conversion_rates().get("BRL");
+
+                    conversionExitosa = consulta.conversionMoneda("USD",
+                            "BRL",
+                            cantidad,
+                            valorMoneda);
+
+                    System.out.println(conversionExitosa);
+                    break;
+                case 4:
+                    System.out.println(solicitudValor);
+                    cantidad = teclado.nextDouble();
+
+                    solicitarConversion = consulta.solicitarConversionMoneda("BRL");
+                    valorMoneda = solicitarConversion.conversion_rates().get("USD");
+
+                    conversionExitosa = consulta.conversionMoneda("BRL",
+                            "USD",
+                            cantidad,
+                            valorMoneda);
+
+                    System.out.println(conversionExitosa);
+                    break;
+                case 5:
+                    System.out.println(solicitudValor);
+                    cantidad = teclado.nextDouble();
+
+                    solicitarConversion = consulta.solicitarConversionMoneda("USD");
+                    valorMoneda = solicitarConversion.conversion_rates().get("COP");
+
+                    conversionExitosa = consulta.conversionMoneda("USD",
+                            "COP",
+                            cantidad,
+                            valorMoneda);
+
+                    System.out.println(conversionExitosa);
+                    break;
+                case 6:
+                    System.out.println(solicitudValor);
+                    cantidad = teclado.nextDouble();
+
+                    solicitarConversion = consulta.solicitarConversionMoneda("COP");
+                    valorMoneda = solicitarConversion.conversion_rates().get("USD");
+
+                    conversionExitosa = consulta.conversionMoneda("COP",
+                            "USD",
+                            cantidad,
+                            valorMoneda);
+
+                    System.out.println(conversionExitosa);
+                    break;
+                case 7:
+                    System.out.println("Sesión finalizada. Gracias por utilizar nuestro conversor de monedas!!");
+                    break;
+
+                default:
+                    System.out.println("Opción ingresada no válida");
+                    break;
+            }
+        }
+
     }
 }
